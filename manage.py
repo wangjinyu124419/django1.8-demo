4cf298c84a60887022cfaea122313ed384253eb9
wangjinyu <wangjinyu124419@gmail.com>
2019-06-10 18:08:30 +0800

    ORM基本操作

--- manage.py
+++ manage.py
@@ -8,7 +8,6 @@ if __name__ == "__main__":
     #原因
     #https://stackoverflow.com/questions/28489863/why-is-run-called-twice-in-the-django-dev-server
     print "*"*100
-    print('123')
     # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     # sys.path.append(BASE_DIR)
     # sys.path.append('/Users/wangjinyu/PycharmProjects/mysite')
