54b9417a26df12aa6137a968b31a1715dbb34fce
wangjinyu <wangjinyu124419@gmail.com>
2019-06-19 20:08:30 +0800

    重写admin的get_urls方法

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
+from django.conf.urls import url
 from django.contrib import admin
 
 # from polls.models import Question
+from django.http import HttpResponse
+from django.template.response import TemplateResponse
+
 from .models import Question
 
 #默认相对导入
@@ -37,7 +41,8 @@ class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
     # fieldsets控制添加数据是显示的字段
 
-
+    #如果不注释这句代码，页面不显示actions，很奇怪
+    show_full_result_count = False
 
     fieldsets = [
         ('target', {'fields': ['question_text','t','year_in_school','push_entire']}),
@@ -59,7 +64,26 @@ class QuestionAdmin(admin.ModelAdmin):
         pass
     export_items_csv.short_description = "Export Selected as CSV"
 
-admin.site.register(Question, QuestionAdmin)
+    #重写get_urls方法，自定义admin路由
+    def get_urls(self):
+        urls = super(QuestionAdmin, self).get_urls()
+        my_urls = [
+            url(r'^myview/$', self.my_view),
+        ]
+        print my_urls + urls
+        return my_urls + urls
 
+    def my_view(self, request):
+        # ...
+        context = dict(
+           # Include common variables for rendering the admin template.
+           self.admin_site.each_context(request),
+           # Anything else you want in the context...
+           key='get_url',
+        )
+        return HttpResponse('override get url')
+        # return TemplateResponse(request, "sometemplate.html", context)
+
+admin.site.register(Question, QuestionAdmin)
 # admin.site.register(Question)
 admin.site.register(Choice)
